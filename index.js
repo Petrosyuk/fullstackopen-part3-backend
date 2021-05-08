b5a604c53a2f33ffd78b04db22aa9b3d5596f3bb
Anton Petrosyuk <antonpetrosyuk@Antons-Air.lan>
2021-05-07 23:14:55 -0400

    Add heroku procfile

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Middleware
 app.use(express.json());
-// app.use(morgan("tiny"));
+app.use(cors());
 
 app.use(
   morgan(function (tokens, req, res) {
